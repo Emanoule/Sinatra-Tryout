72e6105d64b2bd5816e81f7b43cc574f1c858b54
lDiercks <diercks.me@gmail.com>
2012-09-29 21:02:29 +0200

    thisissparta

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require 'rubygems'  
 require 'sinatra'  
 require 'haml'  
-require 'datamapper'
 
 get '/' do
 	haml :index
@@ -10,7 +9,6 @@ end
 get '/new' do
 	haml :createlp
 end
-
 
 get '/*' do
 	haml :error
